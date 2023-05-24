45ad375d0af55dccb329a5cdab26e4148c47c652
Jenna Dornquast <jenna_aspen@hotmail.com>
2023-05-24 17:21:27 -0600

    add movie images and colored rows

--- my-new-app/ClientApp/src/components/RankItems.js
+++ my-new-app/ClientApp/src/components/RankItems.js
@@ -1,4 +1,6 @@
 import React, {useState, useEffect } from 'react';
+import MovieImageArr from './MovieImages.js';
+import RankingGrid from './RankingGrid.js';
 
 export const RankItems = () => {
 
@@ -16,9 +18,19 @@ export const RankItems = () => {
     },[])
 
     return(
-        <main>{
-            (items != null)?items.map((item) => <h3>{item.title}</h3>):<div>Loading...</div>
+        <main>
+            <RankingGrid items={items} imgArr={MovieImageArr}/>
+            <div className='items-not-ranked'>
+            {
+            (items.length > 0) ? items.map((item) => 
+
+            <div className='unranked-cell'>
+                <img id={`item-${item.id}`} src={MovieImageArr.find(o => o.id === item.imageId)?.image }/>
+            </div>
+
+            ):<div>Loading...</div>
             }
+            </div>
         </main>
     )
 }
